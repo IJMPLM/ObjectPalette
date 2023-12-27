a9a83b5e1b1ef93af03613e0de2e161801195573
IJMPLM <iwagjohnmichael@gmail.com>
2023-12-27 22:02:16 +0800

    all from pc

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const GenerativeAI = require('./generativeAI'); // Update the path
+
+const generativeAI = new GenerativeAI();
+
 async function copyText(text) {
   try {
     await navigator.clipboard.writeText(text);
@@ -6,3 +10,31 @@ async function copyText(text) {
     console.error('Unable to copy text: ', err);
   }
 }
+
+async function updateColorPalette(colors) {
+  const objectInput = document.getElementById('objectInput').value;
+  const prompt = `Object: ${objectInput}`;
+
+  const response = await fetch('http://localhost:3000/generateColors', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ prompt }),
+  });
+
+  const result = await response.json();
+  console.log(result.hexCodes);
+
+  // Update the color palette with the received hex codes
+  // ... (remaining code)
+}
+
+document.querySelector('form').addEventListener('submit', async function (e) {
+  e.preventDefault();
+  const objectInput = document.getElementById('objectInput').value;
+  const prompt = `Object: ${objectInput}`;
+
+  const colors = await generativeAI.generateColors(prompt);
+  updateColorPalette(colors);
+});
